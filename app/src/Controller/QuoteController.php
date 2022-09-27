7c8a400b43f3ee94b4fbd7943fb4649e51614c65
lauriane <lourecati1@gmail.com>
2022-09-27 12:14:14 +0200

    bonne question (vincent)

--- app/src/Controller/QuoteController.php
+++ app/src/Controller/QuoteController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Repository\QuoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuoteController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/quotes', name: 'index', methods: [Request::METHOD_GET])]
     public function index(
         QuoteRepository $quoteRepository
     )

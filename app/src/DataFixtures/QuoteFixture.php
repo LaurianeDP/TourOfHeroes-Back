a2e301df1567d926f30c70867f8fcdc686c0edf7
lauriane <lourecati1@gmail.com>
2022-09-27 11:28:23 +0200

    immplementation of composer and symfony, created first database data, controller and template

---
+++ app/src/DataFixtures/QuoteFixture.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Quote;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class QuoteFixture extends Fixture
+{
+    private \Faker\Generator $faker;
+
+    public function __construct() {
+        $this->faker = Factory::create();
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+      for($i=0; $i < 50; $i++) {
+          $manager->persist($this->getQuote());
+      }
+        $manager->flush();
+    }
+
+    private function getQuote():Quote {
+        return new Quote(
+            $this->faker->sentence(5),
+            $this->faker->name(),
+            $this->faker->year()
+        );
+    }
+}
